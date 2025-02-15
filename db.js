8622924203339a6304700a2511e61c524454bb12
HieuBuiCong <ongconghieu1997@gmail.com>
2025-02-15 18:55:48 +0700

    Update db.js

--- db.js
+++ db.js
@@ -1,14 +1,12 @@
 import { Pool } from "pg";
-import dotenv from "dotenv";
-
-dotenv.config();
+import config from "./dotenv.config.js";
 
 const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASS,
-  port: process.env.DB_PORT,
+  user: config.db.user,
+  host: config.db.host,
+  database: config.db.name,
+  password: config.db.password,
+  port: config.db.port,
 });
 
 pool.connect()
