d93907269a83109ad2a93291cd88f37a9b18b742
HieuBuiCong <ongconghieu1997@gmail.com>
2025-02-16 00:19:42 +0700

    Update db.js

--- db.js
+++ db.js
@@ -1,14 +1,7 @@
-CREATE TABLE cid (
-    cid_id SERIAL PRIMARY KEY,
-    product_id INT NOT NULL,
-    prev_rev VARCHAR(50),
-    next_rev VARCHAR(50) NOT NULL,
-    change_notice VARCHAR(255),
-    supplier_id INT,
-    rework_or_not BOOLEAN,
-    OTS_or_not BOOLEAN,
-    sending_date DATE NOT NULL,
-    closing_date DATE,
-    note TEXT,
-    FOREIGN KEY (product_id) REFERENCES product(product_id) ON DELETE CASCADE
+CREATE TABLE product (
+    product_id SERIAL PRIMARY KEY,
+    model VARCHAR(100) NOT NULL,
+    part_number VARCHAR(100) NOT NULL UNIQUE,
+    part_name VARCHAR(100) NOT NULL,
+    owner VARCHAR(255) NULL
 );
