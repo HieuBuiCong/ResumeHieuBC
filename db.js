69a2d8d0368dbdd399e8a740e868ee73b622dcc8
HieuBuiCong <ongconghieu1997@gmail.com>
2025-02-16 02:28:56 +0700

    Update db.js

--- db.js
+++ db.js
@@ -1,22 +1,4 @@
-import nodemailer from "nodemailer";
-
-const transporter = nodemailer.createTransport({
-  service: "gmail",
-  auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS
-  }
-});
-
-export const sendEmail = async (to, subject, text) => {
-  try {
-    await transporter.sendMail({
-      from: process.env.EMAIL_USER,
-      to,
-      subject,
-      text
-    });
-  } catch (error) {
-    console.error("Error sending email:", error);
-  }
-};
+ALTER TABLE cid_task 
+ADD COLUMN approval_date TIMESTAMP,
+ADD COLUMN task_approver_id INT,
+ADD CONSTRAINT fk_task_approver FOREIGN KEY (task_approver_id) REFERENCES users(user_id) ON DELETE SET NULL;
