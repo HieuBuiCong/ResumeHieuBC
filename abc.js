c2bf83878f3f906ebe744a5ee32f33662e2a6486
HieuBuiCong <ongconghieu1997@gmail.com>
2025-03-06 21:19:57 +0700

    Update abc.js

--- abc.js
+++ abc.js
@@ -1,15 +1,16 @@
-import { BrowserRouter } from 'react-router-dom';
-import AppRoutes from './routes/AppRoutes';
-import AuthProvider from './context/AuthContext';
+import React from 'react';
+import LoginForm from '../components/Auth/LoginForm';
 
-function App() {
+const AuthPage = () => {
   return (
-    <AuthProvider>
-      <BrowserRouter>
-        <AppRoutes />
-      </BrowserRouter>
-    </AuthProvider>
+    <div className="h-screen flex justify-center items-center bg-gray-100">
+      <div className="w-full max-w-md p-6 bg-white shadow-md rounded-lg">
+        <h2 className="text-2xl font-semibold text-center mb-4">Login</h2>
+        <LoginForm />
+      </div>
+    </div>
   );
-}
+};
 
-export default App;
+export default AuthPage;
+
