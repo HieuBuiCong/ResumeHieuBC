c78fae5412beb6c7c89085da30be481ca9077dcb
HieuBuiCong <ongconghieu1997@gmail.com>
2025-03-08 14:38:19 +0700

    Update controller.js

--- controller.js
+++ controller.js
@@ -1,23 +1,28 @@
 import React, { useEffect, useState } from 'react';
 import { Toast, ToastContainer } from 'react-bootstrap';
 
-const Error = ({ message, onClose }) => {
+const Error = ({ message, type = "danger", onClose }) => {
   const [show, setShow] = useState(true);
 
   useEffect(() => {
     const timer = setTimeout(() => {
-      setShow(false); // Auto-close after 2 seconds
+      setShow(false);
       if (onClose) onClose();
     }, 2000);
 
-    return () => clearTimeout(timer); // Cleanup timer
+    return () => clearTimeout(timer);
   }, [onClose]);
 
   return (
     <ToastContainer position="top-end" className="p-3">
-      <Toast show={show} bg="danger" onClose={() => setShow(false)} delay={2000} autohide>
+      <Toast show={show} bg={type} onClose={() => setShow(false)} delay={2000} autohide>
         <Toast.Header>
-          <strong className="me-auto text-white">Error</strong>
+          <strong className="me-auto text-white">
+            {type === "danger" && "Error"}
+            {type === "success" && "Success"}
+            {type === "warning" && "Warning"}
+            {type === "info" && "Information"}
+          </strong>
         </Toast.Header>
         <Toast.Body className="text-white">{message}</Toast.Body>
       </Toast>
