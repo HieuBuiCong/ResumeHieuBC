03a270c61bc828b6ae404dda6639d17a7f88f955
HieuBuiCong <ongconghieu1997@gmail.com>
2025-03-20 14:54:46 +0700

    Update abc.js

--- abc.js
+++ abc.js
@@ -1,35 +1,49 @@
 import React from "react";
-import useTableLogic from "../../hooks/useTableLogic";
-import ReusableTable from "../common/ReusableTable";
-import { getTaskCategoryData, taskCategoryDelete, taskCategoryUpdate } from "../../services/taskCategoryService";
-import TaskCategoryRegisterForm from "./TaskCategoryRegisterForm";
+import useTableLogic from "./useTableLogic.js";
+import ReusableTable from "./ReusableTable.jsx";
+import { getTaskQuestionData, taskQuestionDelete, taskQuestionUpdate } from "../../services/taskQuestionService";
+import TaskCategoryRegisterForm from "../TaskCategoryAndQuestion/TaskCategoryRegisterForm.jsx";
 import { useDarkMode } from "../../context/DarkModeContext";
 import { createTheme } from "@mui/material";
 
-const columns = ["task_category_id", "task_name"];
+const columns = ["task_category_question_id", "question_name"];
 
-const TaskCategoryTable = () => {
-  const { darkMode } = useDarkMode();
-  const theme = createTheme({ palette: { mode: darkMode ? "dark" : "light" } });
+const TaskQuestionTableS = ({selectedTaskCategoryForQuestion, setSelectedTaskCategoryForQuestion}) => {
+
+    // 🌑🌚🌚🌚🎯 dark mode
+    const { darkMode } = useDarkMode();
+    const theme = createTheme({
+        palette: {
+        mode: darkMode ? "dark" : "light",
+        primary: { main: darkMode ? "#90caf9" : "#1976d2" },
+        background: { default: darkMode ? "#121212" : "#f8f9fa" },
+        backgroundColor : {default: darkMode ? "rgba(33, 31, 31, 0.7)" : "rgba(255,255,255,0.7)"},
+        text: { primary: darkMode ? "#ffffff" : "#000000" },
+        },
+    });
 
   const logic = useTableLogic({
-    fetchDataFn: getTaskCategoryData,
-    identifierKey: "task_category_id",
-    deleteFn: taskCategoryDelete,
-    updateFn: taskCategoryUpdate,
-    itemLabelKey: "task_name",
+    fetchDataFn: getTaskQuestionData,
+    identifierKey: "task_category_question_id",
+    deleteFn: taskQuestionDelete,
+    updateFn: taskQuestionUpdate,
+    itemLabelKey: "question_name",
+    filterCondition: (item) => selectedTaskCategoryForQuestion.task_category_id === item.task_category_id,
   });
 
   return (
     <ReusableTable
-      logic={logic}
-      columns={columns}
-      identifierKey="task_category_id"
-      title="Task Categories"
-      RegisterFormComponent={TaskCategoryRegisterForm}
-      theme={theme}
+        logic={logic}
+        columns={columns}
+        identifierKey="task_category_question_id"
+        selectedItemByOtherTableFiltering={selectedTaskCategoryForQuestion} 
+        setSelectedItemByOtherTableFiltering={setSelectedTaskCategoryForQuestion}
+        identifierKeyOfFilteringTable="task_category_id"
+        title="Question"
+        RegisterFormComponent={TaskCategoryRegisterForm}
+        theme={theme}
     />
   );
 };
 
-export default TaskCategoryTable;
+export default TaskQuestionTableS;
